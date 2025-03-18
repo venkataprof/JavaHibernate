d29465a7bf305d36bce616f96ecdf7eda916aab8
venkataprof <venkataprof@outlook.com>
2025-03-18 13:48:06 -0700

    setting up maven quickstart project for hibernate

---
+++ src/main/java/com/venkataprof/learnHibernate/App.java
@@ -0,0 +1,13 @@
+package com.venkataprof.learnHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
